1f44c3df9f7d50f23023717086055b006db2692b
Wesley de Paula <wesley.paula@omegaenergia.com.br>
2023-05-11 20:28:45 -0300

    Generate logs from api gatway

--- lib/ecommerceApi-stack.ts
+++ lib/ecommerceApi-stack.ts
@@ -12,8 +12,23 @@ export class ECommerceApiStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props: ECommerceApiStackProps) {
         super(scope, id, props);
 
+        const logGroup = new cwlogs.LogGroup(this, 'ECommerceApiLogs');
         const api = new apiGateway.RestApi(this, 'ECommerceApi', {
             restApiName: 'ECommerceApi',
+            deployOptions: {
+                accessLogDestination: new apiGateway.LogGroupLogDestination(logGroup),
+                accessLogFormat: apiGateway.AccessLogFormat.jsonWithStandardFields({
+                    httpMethod: true,
+                    ip: true,
+                    protocol: true,
+                    requestTime: true,
+                    resourcePath: true,
+                    responseLength: true,
+                    status: true,
+                    caller: true,
+                    user: true,
+                }),
+            },
         });
 
         const productsFetchIntegration = new apiGateway.LambdaIntegration(props.productsFetchHandler);
